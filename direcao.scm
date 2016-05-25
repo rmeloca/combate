7da55346fd51b4f4789b9b0ce130d703627b0b34
Rômulo Manciola Meloca <rmeloca@gmail.com>
2016-05-25 03:51:43 -0300

    renamed, gitignore, tabuleiro

---
+++ direcao.scm
@@ -0,0 +1,6 @@
+#lang scheme
+
+(define NORTH 0)
+(define SOUTH 1)
+(define WEST 2)
+(define EAST 3)
---
+++ main.scm
@@ -0,0 +1,17 @@
+#lang scheme
+
+(include "tabuleiro.scm")
+(include "direcao.scm")
+
+;initialize
+;while !win
+;input
+;canMove
+;move
+
+;initialize
+
+"Coordenadas x e y"
+(print (list (read) (read)))
+"Direção"
+(read)
