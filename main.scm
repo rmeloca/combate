90ee27c82aa22b9da72160c3330bf65412c63cf3
Rômulo Manciola Meloca <rmeloca@gmail.com>
2016-05-25 20:07:16 -0300

    a

--- main.scm
+++ main.scm
@@ -1,17 +1,38 @@
 #lang scheme
 
-(include "tabuleiro.scm")
-(include "direcao.scm")
+(require "tabuleiro.scm")
 
-;initialize
-;while !win
-;input
-;canMove
-;move
+tabuleiro
 
-;initialize
+;define método que opera sobre um tabuleiro atribuindo uma estratégia randômica
+(define (initialize tabuleiro)
+	(list (random 11))
+)
 
-"Coordenadas x e y"
-(print (list (read) (read)))
-"Direção"
-(read)
+;define mudança de turno
+(define (modificarTurno turno)
+	null;
+)
+
+;define o motor do jogo. laço principal
+(define (motor tabuleiro turno)
+	(if
+		(haveWinner tabuleiro)
+		(print "Vencedor")
+		(motor
+			(move
+				(canMove
+					(if
+						(eq? turno 1)
+						(coordenada (read) (read)) (read) tabuleiro
+						(heuristica tabuleiro)
+					)
+				)
+			)
+			(modificarTurno turno)
+		)
+	)
+)
+
+;executa o jogo
+(motor (initialize tabuleiro))
--- tabuleiro.scm
+++ tabuleiro.scm
@@ -1,5 +1,6 @@
 #lang scheme
 
+;define constantes enumeradas
 (define TERRITORIO 0)
 (define BANDEIRA 1)
 (define SOLDADO 2)
@@ -12,6 +13,7 @@
 (define GENERAL 9)
 (define BOMBA 10)
 
+;define uma instância de teste
 (define instanciaTeste
 	(list
 		(list GENERAL SOLDADO BANDEIRA)
@@ -20,6 +22,7 @@
 	)
 )
 
+;define a instância inicial de um tabuleiro sem estratégia montada
 (define tabuleiro
 	(list
 		(list TERRITORIO TERRITORIO TERRITORIO TERRITORIO TERRITORIO TERRITORIO TERRITORIO TERRITORIO TERRITORIO TERRITORIO)
@@ -36,17 +39,47 @@
 	)
 )
 
-;void addElemento(tabuleiro, elemento)
-;int getQuantidadeElementos(tabuleiro, elemento)
-;void initializeTabuleiro(tabuleiro)
+;retorna uma nova lista com o elemento dado por parâmetro adicionado ao final desta
+(define (addElemento tabuleiro elemento)
+	null
+)
 
+;obtém o tamanho do tabuleiro
+(define (size tabuleiro)
+	null
+)
+
+;define as coordenadas
+(define (coordenada x y)
+	(list x y)
+)
+
+;retorna boolean se é possível mover a peça para o direção dada
+(define (canMove coordenada direcao tabuleiro)
+	null
+)
+
+;calcula a jogada do computador
+(define (heuristica)
+	null
+)
+
+;retorna boolean se há vencedor
+(define (haveWinner tabuleiro)
+	#t
+)
+
+;faz o ataque
+(define (atacar)
+	null
+)
+
+;obtém o número de vezes que o elemento aparece no tabuleiro
 (define (getQuantidadeElementos tabuleiro elemento)
 	(cond 
 		((not (list? tabuleiro)) null)
 		((null? tabuleiro) null)
-                ((equal? (car tabuleiro) elemento) 1)
+        ((equal? (car tabuleiro) elemento) 1)
 		(else (+(1 (getQuantidadeElementos (car(tabuleiro) elemento)))))
 	)
 )
-
-(random 11)
